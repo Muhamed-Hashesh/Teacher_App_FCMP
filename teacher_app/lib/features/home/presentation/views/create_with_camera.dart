62c2849e3fa308d6dc92409f4ee05360f62be464
Muhamed-Hashesh <muhamed7as4h@gmail.com>
2024-11-17 23:36:15 +0200

    start creating camera

---
+++ teacher_app/lib/features/home/presentation/views/create_with_camera.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CreateWithCameraPage extends StatelessWidget {
+  const CreateWithCameraPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
